57b8cc393473855470c6ca4f8bc5520beecfb047
mauricio sánchez barragán <mausanbar93@gmail.com>
2016-09-14 14:02:34 -0500

    Declarando adecuadamente el modelo users
    Para mejorar la forma de declarar el modelo users, se declara el metodo
    model en el modelo "users" y se declara su parentesco.

--- tutorial/protected/controllers/HolaController.php
+++ tutorial/protected/controllers/HolaController.php
@@ -1,10 +1,14 @@
 <?php 
-
+/*
+* Acceder al holamundo con la siguiente url:
+* http://localhost/yii/proyectos-YII/tutorial/hola/index
+*/
 class HolaController extends Controller
 {
 	public function actionIndex()
 	{
-		$model=CActiveRecord::model("Users")->findAll();
+		//$model=CActiveRecord::model("Users")->findAll();
+		$model=Users::model()->findAll();
 		$twitter="@maurosanchez93";
 		$this->render("index",array("model"=>$model,"twitter"=>$twitter));
 	}
